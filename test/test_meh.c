acb18c01a1bd3ab94abb4b23ece74574942af219
Maximilian Fischer <maximilian.fischer@uni-due.de>
2021-08-18 09:33:33 +0200

    adds passing test

--- test/test_meh.c
+++ test/test_meh.c
@@ -13,11 +13,17 @@ void test_shouldFail(void)
 	TEST_FAIL();
 }
 
+void test_shouldPass(void)
+{
+	TEST_PASS();
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
 
 	RUN_TEST(test_shouldFail);
+	RUN_TEST(test_shouldPass);
 
 	return UNITY_END();
 }
